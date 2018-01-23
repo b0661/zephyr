46793c030a467c7ff4b0b3dc4393c152f14f6e70
Bobby Noelte <b0661n0e17e@gmail.com>
2018-01-23 18:58:42 +0100

    drivers: pinctrl: Add STM32 pinctrl driver and DTS
    A kernel configuration and build environment for
    pinctrl drivers.
    
    Add device tree bindings for the STM32 pin controller.
    
    Add STM32 pin controller driver. The driver uses the
    pinctrl library to implement the generic driver
    functionality.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- arch/arm/soc/st_stm32/stm32f0/soc.h
+++ arch/arm/soc/st_stm32/stm32f0/soc.h
@@ -43,6 +43,10 @@
 #include <stm32f0xx_ll_system.h>
 #endif /* CONFIG_CLOCK_CONTROL_STM32_CUBE */
 
+#ifdef CONFIG_PINCTRL_STM32
+#include <stm32f0xx_ll_gpio.h>
+#endif
+
 #ifdef CONFIG_IWDG_STM32
 #include <stm32f0xx_ll_iwdg.h>
 #endif
