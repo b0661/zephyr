87cad3b559ee46708ee76d3912f6cd54a7359052
Bobby Noelte <b0661n0e17e@gmail.com>
2018-05-09 18:50:59 +0200

    dts: stm32f0: update nodes for driver configuration
    Enable STM32 driver configuration by device tree.
    Set st,gpio-bank for gpio nodes.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

--- drivers/spi/spi_ll_stm32_python.h.py
+++ drivers/spi/spi_ll_stm32_python.h.py
@@ -1,3 +1,9 @@
+#
+# Copyright (c) 2018 Bobby Noelte
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 cogeno.import_module('devicedeclare')
 
 device_configs = ['CONFIG_SPI_{}'.format(x) for x in range(1, 7)]
